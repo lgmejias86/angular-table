d1227b1aa67249f328b1715a645c3375bb72127a
lgmejias86 <lgarciamejias@gmail.com>
2020-09-15 16:59:42 -0400

    remove static data of testing table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -11,21 +11,6 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-// TODO remove later
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-const EMPLOYEE_DATA: Employee[] = [];
 
 @Component({
   selector: 'app-table',
@@ -35,7 +20,7 @@ const EMPLOYEE_DATA: Employee[] = [];
 export class TableComponent implements OnInit, OnDestroy {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'select'];
-  dataSource = EMPLOYEE_DATA;
+  dataSource: Employee[] = [];
 
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
   myselectedFoods = ['Extra cheese', 'Mushroom'];
